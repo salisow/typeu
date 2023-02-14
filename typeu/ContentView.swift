9a8ff509b387c69eb874691f5701556d0791c801
typeu <djtypeu@post.com>
2023-02-14 11:39:52 -0500

    sync xcode files and gitignore

---
+++ typeu/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  typeu
+//
+//  Created by Dj Typeu on 2/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Diadieuf")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ typeu/typeuApp.swift
@@ -0,0 +1,17 @@
+//
+//  typeuApp.swift
+//  typeu
+//
+//  Created by Dj Typeu on 2/14/23.
+//
+
+import SwiftUI
+
+@main
+struct typeuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
